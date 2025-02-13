7b0eea2604c555149fad6872169e8485aaf12c31
Chris Coomber <ccoomber@whiterosemaths.com>
2025-02-13 01:08:35 +0000

    Example of workaround

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { useMemo, useRef } from 'react';
 import {  Text, View, Animated, PanResponder } from "react-native";
 import { Svg, Rect } from 'react-native-svg';
 import { GestureDetector, Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
-import RNRAnimated, { useSharedValue, useAnimatedProps} from 'react-native-reanimated'
+import RNRAnimated, { useSharedValue, useAnimatedProps, useAnimatedStyle} from 'react-native-reanimated'
 
 const RNRAnimatedRect = RNRAnimated.createAnimatedComponent(Rect)
 const AnimatedRect = Animated.createAnimatedComponent(Rect)
@@ -32,6 +32,7 @@ export default function Index() {
 
   const x = useSharedValue(0)
   const animatedX = useAnimatedProps(() => ({x: x.value}), [x]);
+  const animatedLeft = useAnimatedStyle(() => ({left: x.value}), [x])
   const xStart = useSharedValue(0);
   const svgPanGesture = useMemo(() => {
     return Gesture.Pan()
@@ -44,12 +45,15 @@ export default function Index() {
   const RNGHTest = <View style={{borderWidth: 1}}>
     <Text>With RNGH and RNR</Text>
 
-    <Svg width={200} height={100}>
-    <Rect width={200} height={100} fill='yellow'/>
-      <GestureDetector gesture={svgPanGesture}>
+    <View style={{width: 200, height: 100}}>
+      <Svg width={200} height={100}>
+        <Rect width={200} height={100} fill='yellow'/>
         <RNRAnimatedRect width={100} height={100} animatedProps={animatedX} y={0} fill='red' />
-      </GestureDetector>        
-    </Svg>
+      </Svg>
+      <GestureDetector gesture={svgPanGesture}>
+        <RNRAnimated.View style={[{position: 'absolute', width: 100, height: 100}, animatedLeft]}/>
+      </GestureDetector>
+    </View>
   </View>
 
   return (
